f1359e646a53b3690177d7b54c5ee5806aaec0bf
Allan James Lapid <ajLapid718@gmail.com>
2023-06-20 14:58:33 -0400

    change vercel file config

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ const startListening = () => {
   })
 };
 
-// const creditsRouter = require("express").Router();
-// const debitsRouter = require("express").Router();
-
 app.get("/credits", (req, res, next) => {
   res.status(200).json(65330);
 });
@@ -19,28 +16,7 @@ app.get("/debits", (req, res, next) => {
   res.status(200).json(12500);
 });
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
 const bootApp = async () => {
-  await allowCors(handler);
   await startListening();
 };
 
